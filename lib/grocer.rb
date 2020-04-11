ac17ae7527dcc5b5b8732b8dcb65836e5cbaa7cf
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-11 15:02:29 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   collection.length.times do |i|
     if(collection[i][:item]==name)
-      return collection[i]
+      puts collection[i]
     else
       return nil
     end
